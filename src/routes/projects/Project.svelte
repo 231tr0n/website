545c4271a1f87a8267770ce8ff423d2126c8844d
231tr0n <zeltronsrikar@gmail.com>
2024-02-15 01:35:33 +0530

    test

--- src/routes/projects/Project.svelte
+++ src/routes/projects/Project.svelte
@@ -10,16 +10,18 @@
 <h1>Projects</h1>
 {#await fetch_url_data('https://api.github.com/users/231tr0n/repos') then res}
   {#each Object.values(res) as repo}
-    {#await fetch_url_data(repo['languages_url']) then res}
-      <Projectpost
-        name={repo['name']}
-        badges={Object.keys(res)}
-        description={repo['description']}
-        url={repo['html_url']}
-      />
-    {:catch error}
-      <div class="error">{error}</div>
-    {/await}
+    {#if !repo['fork']}
+      {#await fetch_url_data(repo['languages_url']) then res}
+        <Projectpost
+          name={repo['name']}
+          badges={Object.keys(res)}
+          description={repo['description']}
+          url={repo['html_url']}
+        />
+      {:catch error}
+        <div class="error">{error}</div>
+      {/await}
+    {/if}
   {/each}
 {:catch error}
   <div class="error">{error}</div>
